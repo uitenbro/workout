c7a1e46efd35424f7c91c601e8b3ed81e6c9d78d
Nate Uitenbroek <nate.uitenbroek@nasa.gov>
2018-12-12 15:59:15 -0600

    fixed set display options and created Today button

--- workout.js
+++ workout.js
@@ -4,8 +4,8 @@ var workoutData = {};
 function readStoredData() {
     workoutDay = JSON.parse(localStorage.getItem("workoutDay"));
     workoutData = JSON.parse(localStorage.getItem("workoutData"));
-    console.log ("readStoredData");
-    console.log (workoutDay, workoutData);
+    //console.log ("readStoredData");
+    //console.log (workoutDay, workoutData);
 }
 
 function updateStoredData(item, value) {
@@ -30,7 +30,45 @@ function initializeStoredData () {
     //console.log('create default workoutDay');
     localStorage.setItem('workoutDay', workoutDay);
   }
+
 }
+
+function clearStoredData() {
+
+  if (localStorage.removeItem('workoutData')) {
+    //console.log('found stored workoutData');
+  }
+  if (localStorage.removeItem('workoutDay')) {
+    //console.log('found stored workoutDay');
+  }
+}
+
+/*
+<html>
+<body>
+
+<h2>Use the XMLHttpRequest to get the content of a file.</h2>
+<p>The content is written in JSON format, and can easily be converted into a JavaScript object.</p>
+
+<p id="demo"></p>
+
+<script>
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.name;
+  }
+};
+xmlhttp.open("GET", "json_demo.txt", true);
+xmlhttp.send();
+</script>
+
+<p>Take a look at <a href="json_demo.txt" target="_blank">json_demo.txt</a></p>
+
+</body>
+</html>
+*/
 
 function init () {
     initializeStoredData();
@@ -70,8 +108,8 @@ function printHeader (dayNum) {
     var secondButton = document.createElement('a');
     secondButton.className = "action";
     secondButton.id = "secondButton";
-    secondButton.href="javascript:resetApp();";
-    secondButton.appendChild(document.createTextNode("Reset"));
+    secondButton.href="javascript:displayDay("+workoutDay+");";
+    secondButton.appendChild(document.createTextNode("Today"));
     header.appendChild(secondButton);
 
     var thirdButton = document.createElement('a');
@@ -141,24 +179,21 @@ function printWorkoutInfo(dayNum) {
     var workoutLi = document.createElement('li');
     
     var workoutName = document.createElement('a');
-    workoutName.href = workoutData.url;
-    workoutName.target = "_blank";
+    workoutName.href = "javascript:showWorkoutOptions("+dayNum+");"
     workoutName.className = "main";
     workoutName.appendChild(document.createTextNode(workoutData.workoutName));
     workoutLi.appendChild(workoutName);
 
     var dayNumber = document.createElement('a');
     dayNumber.className = "right";
-    dayNumber.href = workoutData.url;
-    dayNumber.target = "_blank";
+    dayNumber.href = "javascript:showWorkoutOptions("+dayNum+");"
     var printDay = dayNum+1;
     dayNumber.appendChild(document.createTextNode("Day " + printDay))
     workoutLi.appendChild(dayNumber);
 
     var dayName = document.createElement('a');
     var dayData = workoutData.days[dayNum%(workoutData.days.length)];
-    dayName.href = workoutData.url;
-    dayName.target = "_blank";
+    dayName.href = "javascript:showWorkoutOptions("+dayNum+");"
     dayName.className = "";
     dayName.appendChild(document.createTextNode(dayData.dayName));
     workoutLi.appendChild(dayName);
@@ -207,11 +242,12 @@ function printExercise(dayNum, exerNum) {
 
 function completeDay(dayNum) {
     //console.log("Complete");
+    workoutDay = dayNum+1;
     updateStoredData('workoutDay', dayNum+1)
     printHeader(dayNum+1);
     printMain(dayNum+1);
 }
-function resetApp() {
+function resetToday() {
     //console.log("Reset");
     updateStoredData('workoutDay', 0)
     printHeader(0);
@@ -232,10 +268,17 @@ function browseNext(dayNum) {
     printHeader(dayNum+1);
     printMain(dayNum+1);
 }
+function displayDay(dayNum) {
+    //console.log("Next");
+    printHeader(dayNum);
+    printMain(dayNum);
+    closeOptions();
+}
+
 function displayWeightOptions(dayNum, exerNum) {
-    console.log(workoutData)
+    //console.log(workoutData)
     var dayData = workoutData.days[dayNum%(workoutData.days.length)];
-    console.log(dayData)
+    //console.log(dayData)
 
     weightUpdate = document.createElement('div')
     weightUpdate.id = "options";
@@ -294,12 +337,12 @@ function displayWeightOptions(dayNum, exerNum) {
         weightInput.type = "text";
         weightInput.name = "weight["+j+"]";
         weightInput.value = dayData.exercises[exerNum].sets[j].weight;
-        console.log(weightInput.value,dayData.exercises[exerNum].sets[j].weight);
+        //console.log(weightInput.value,dayData.exercises[exerNum].sets[j].weight);
         a.appendChild(weightInput);
         li.appendChild(a);
 
         var a = document.createElement('a');
-        a.appendChild(document.createTextNode(dayData.exercises[i].sets[j].label))
+        a.appendChild(document.createTextNode(dayData.exercises[exerNum].sets[j].label));
         li.appendChild(a);
         form.appendChild(li);
     }
@@ -313,7 +356,7 @@ function displayWeightOptions(dayNum, exerNum) {
     // Save
     var save = document.createElement('a');
     save.className = "black button";
-    save.href = "javascript:updateWeights("+dayNum+","+exerNum+");printHeader();printMain("+dayNum+");closeOptions()";
+    save.href = "javascript:updateWeights("+dayNum+","+exerNum+");displayDay("+dayNum+")";
     save.appendChild(document.createTextNode("Save"));
     buttonContainer.appendChild(save);
     weightUpdate.appendChild(buttonContainer);
@@ -326,14 +369,12 @@ function displayWeightOptions(dayNum, exerNum) {
     
 }
 function updateWeights (dayNum, exerNum) {
-    console.log("update weights" + dayNum + " " + exerNum);
+    //console.log("update weights" + dayNum + " " + exerNum);
     dayNum = dayNum%7;
     for (i in workoutData.days[dayNum].exercises[exerNum].sets) {
-        console.log("before "+workoutData.days[dayNum].exercises[exerNum].sets[i].weight)
-        console.log("during "+document.forms['updateWeight']['weight['+i+']'].value)
+        //console.log("during "+document.forms['updateWeight']['weight['+i+']'].value)
         workoutData.days[dayNum].exercises[exerNum].sets[i].weight = document.forms['updateWeight']['weight['+i+']'].value;
         updateStoredData('workoutData', workoutData)
-        console.log("after  "+workoutData.days[dayNum].exercises[exerNum].sets[i].weight)
     }
 }
 function closeOptions() {
@@ -341,3 +382,68 @@ function closeOptions() {
     document.getElementById('main').style.display = 'block';
     document.getElementById('options').style.display = 'none';
 }
+function showWorkoutOptions(dayNum) {
+   
+    // Workout Options
+    workoutOptions = document.createElement('div')
+    workoutOptions.id = "options";
+    workoutOptions.className = "optionpanel";
+    workoutOptions.style.display = "block"
+
+    // Cancel
+    var cancel = document.createElement('a');
+    var img = document.createElement('img');
+    img.src = "images/cancel.png";
+    cancel.appendChild(img);
+    cancel.href = "javascript:closeOptions();";
+    //weightUpdate.appendChild(cancel);
+
+    var h2 = document.createElement('h2');
+    h2.appendChild(cancel);
+    h2.appendChild(document.createTextNode("Workout Options"));
+    workoutOptions.appendChild(h2);
+
+    workoutOptions.appendChild(printWorkoutInfo(dayNum));
+
+    // Action Buttons
+    var buttonContainer = document.createElement('p');
+
+    // Info
+    var info = document.createElement('a');
+    info.className = "black button";
+    info.href = "javascript:window.open('"+workoutData.url+"','_blank');closeOptions();";
+    info.target = "_blank";
+    info.appendChild(document.createTextNode("Workout Info"));
+    buttonContainer.appendChild(info);
+    workoutOptions.appendChild(buttonContainer);
+
+    // Reset Today
+    var info = document.createElement('a');
+    info.className = "black button";
+    info.href = "javascript:resetToday();displayDay(0)";
+    info.appendChild(document.createTextNode("Restart Today"));
+    buttonContainer.appendChild(info);
+    workoutOptions.appendChild(buttonContainer);
+
+    // Reset Today
+    var info = document.createElement('a');
+    info.className = "black button";
+    info.href = "javascript:resetWorkout();displayDay(0)";
+    info.appendChild(document.createTextNode("Reset Workout"));
+    buttonContainer.appendChild(info);
+    workoutOptions.appendChild(buttonContainer);
+
+    // Add to the page and hide main panel
+    document.getElementById('options').replaceWith(workoutOptions);
+    document.getElementById('header').style.display = 'none';
+    document.getElementById('main').style.display = 'none';
+    window.scrollTo(0, 0);
+
+}
+
+function resetWorkout() {
+    clearStoredData();
+    init();
+}
+
+
