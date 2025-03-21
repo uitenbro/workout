3f4e6794bff39a27cfeffa994e3ead0ac201ecd4
Nathan Uitenbroek <nathanu@gmail.com>
2025-03-21 11:53:52 -0500

    udpate secure server setup

--- devHttpsServer.py
+++ devHttpsServer.py
@@ -4,8 +4,10 @@ from http.server import CGIHTTPRequestHandler
 
 httpd = http.server.HTTPServer(('127.0.0.1', 8000), CGIHTTPRequestHandler)
 
-# Use the ssl module to enable HTTPS
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./localhost.pem', keyfile='./localhost-key.pem', server_side=True)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile='./localhost.pem', keyfile='./localhost-key.pem')
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print("Serving on https://localhost:8000/")
 httpd.serve_forever()
